0dc9fbdeb5c39f142657bd55b1a5e22520d24905
Jose De La Rosa <55119652+SrParadoja@users.noreply.github.com>
2026-04-14 10:48:58 -0500

    Correción minuscula

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 for numeromisterioso in range(1, 1001):
     if numeromisterioso % 3 == 0 and numeromisterioso % 5 == 0:
-        print("FizzBuzz")
+        print("Fizzbuzz")
     elif numeromisterioso % 3 == 0:
         print("Fizz")
     elif numeromisterioso % 5 == 0:
